e18e78cad0cffebcad73d9e351e5b8c23f7aad80
Erison Quintela de Freitas <erison.freitas@viavarejo.com.br>
2024-05-28 18:22:09 -0300

    feature: create activities migration

---
+++ database/migrations/2024_05_28_210133_create_activities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('type');
+            $table->text('description');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->dateTime('completed_date')->nullable();
+            $table->enum('status', ['open', 'completed']);
+
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
